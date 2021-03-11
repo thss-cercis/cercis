d55e2d8f8275d9c730964a1cbf8502e81e538d68
Rhacoal <rhacoal@gmail.com>
2021-03-11 15:29:47 +0800

    including Stetho

--- app/src/main/java/cn/edu/tsinghua/thss/cercis/CercisApplication.kt
+++ app/src/main/java/cn/edu/tsinghua/thss/cercis/CercisApplication.kt
@@ -1,7 +1,20 @@
 package cn.edu.tsinghua.thss.cercis
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CercisApplication : Application()
+class CercisApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Stetho.initialize(
+                    Stetho.newInitializerBuilder(this)
+                            .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                            .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+                            .build()
+            );
+        }
+    }
+}
