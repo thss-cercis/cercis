f51aab4d4fcaaffe2a47d703a61b29a6cbe933a0
Rhacoal <rhacoal@gmail.com>
2021-09-08 00:25:11 +0800

    added save image

--- app/src/main/java/cn/cercis/util/Utils.kt
+++ app/src/main/java/cn/cercis/util/Utils.kt
@@ -3,13 +3,16 @@ package cn.cercis.util
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.provider.MediaStore
 import androidx.annotation.ColorRes
 import androidx.annotation.StringRes
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import androidx.core.graphics.drawable.toBitmap
 import cn.cercis.CercisApplication
 import java.io.File
 import java.util.concurrent.atomic.AtomicInteger
@@ -30,8 +33,16 @@ fun getTempFile(filename: String, extension: String): File {
 }
 
 private val atomicInt = AtomicInteger(0)
+fun getTempFilename(): String {
+    return "${System.currentTimeMillis()}_${atomicInt.getAndIncrement()}"
+}
+
+fun getTempFilename(extension: String): String {
+    return "${getTempFilename()}.${extension}"
+}
+
 fun getTempFile(extension: String): File {
-    return File.createTempFile("${System.currentTimeMillis()}_${atomicInt.getAndIncrement()}",
+    return File.createTempFile(getTempFilename(),
         extension,
         CercisApplication.application.cacheDir)
 }
@@ -53,3 +64,9 @@ fun setClipboard(context: Context, text: String) {
         ?.setPrimaryClip(ClipData.newPlainText("label", text))
 }
 
+fun saveImage(context: Context, drawable: Drawable, title: String): String? {
+    return MediaStore.Images.Media.insertImage(context.contentResolver,
+        drawable.toBitmap(),
+        title,
+        "")
+}
--- app/src/main/java/cn/cercis/util/helper/VideoDialog.kt
+++ app/src/main/java/cn/cercis/util/helper/VideoDialog.kt
@@ -1,6 +1,7 @@
 package cn.cercis.util.helper
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.view.WindowManager.LayoutParams
 import android.widget.ImageView
@@ -9,8 +10,18 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.net.toUri
 import cn.cercis.R
 import cn.cercis.databinding.DialogPlayVideoBinding
+import cn.cercis.util.getString
+import cn.cercis.util.getTempFilename
+import cn.cercis.util.resource.Resource
+import cn.cercis.util.saveImage
+import cn.cercis.util.snackbarMakeSuccess
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 
 fun showVideoDialog(context: Context, uri: String): AlertDialog {
     return MaterialAlertDialogBuilder(context)
@@ -49,11 +60,50 @@ fun showImageDialog(context: Context, uri: String): AlertDialog {
         .show().apply {
             window?.setLayout(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
             findViewById<ImageView>(R.id.dialog_show_image_view)!!.let { view ->
+                var loadingImage: Resource<Drawable> = Resource.Loading(null)
                 Glide.with(view)
                     .load(uri.toUri())
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean,
+                        ): Boolean {
+                            loadingImage = Resource.Error(-1, "", null)
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean,
+                        ): Boolean {
+                            resource?.let {
+                                loadingImage = Resource.Success(resource)
+                            }
+                            return false
+                        }
+                    })
                     .into(view)
                 view.setOnClickListener {
                     this.cancel()
+                }
+                view.setOnCreateContextMenuListener { menu, _, _ ->
+                    menu.add(R.string.image_save_to_local).setOnMenuItemClickListener {
+                        when (val image = loadingImage) {
+                            is Resource.Success -> {
+                                saveImage(context, image.data, getTempFilename())?.let {
+                                    snackbarMakeSuccess(view,
+                                        getString(R.string.image_save_to_local_success),
+                                        Snackbar.LENGTH_SHORT)
+                                }
+                            }
+                        }
+                        true
+                    }
                 }
             }
         }
